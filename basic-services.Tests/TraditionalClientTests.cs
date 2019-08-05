c3e33110f1a7f81cae9988114f9a19ad0cceb8a3
jevanssp <savannah.pamela.evans@jci.com>
2019-08-05 16:06:45 -0500

    Added test for unauthorized access. Any calls that return a 401/unauthorized-access will throw an exception.

--- basic-services.Tests/TraditionalClientTests.cs
+++ basic-services.Tests/TraditionalClientTests.cs
@@ -8,21 +8,14 @@ namespace Tests
 {
     public class TraditionalClientTests
     {
-        TraditionalClient traditionalClient;
-
-        // [SetUp]
-        // public void Setup()
-        // {
-        //     //traditionalClient = new TraditionalClient();
-        // }
 
         [Test]
         public void TestLogin()
         {
             using (var httpTest = new HttpTest())
             {
-                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2020-01-01T00:00:00Z"});
-                traditionalClient = new TraditionalClient("username", "password", "hostname", 2);
+                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2030-01-01T00:00:00Z"});
+                var traditionalClient = new TraditionalClient("username", "password", "hostname", 2);
                 httpTest.ShouldHaveCalled($"https://hostname/api/v2/login")
                     .WithVerb(HttpMethod.Post)
                     .WithContentType("application/json")
@@ -32,15 +25,57 @@ namespace Tests
         }
 
         [Test]
+        public void TestUnauthorizedLoginThrowsException()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest.RespondWith("unauthorized", 401);
+                try {
+                    var traditionalClient = new TraditionalClient("username", "badpassword", "hostname", 2);
+                    Assert.Fail();
+                } catch {
+                    httpTest.ShouldHaveCalled($"https://hostname/api/v2/login")
+                    .WithVerb(HttpMethod.Post)
+                    .WithContentType("application/json")
+                    .WithRequestBody("{\"username\":\"username\",\"password\":\"badpassword\"")
+                    .Times(1);
+                }
+            }
+        }
+
+        [Test]
         public void TestRefresh()
         {
             using (var httpTest = new HttpTest())
             {
-                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2020-01-01T00:00:00Z"});
+                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2030-01-01T00:00:00Z"});
+                var traditionalClient = new TraditionalClient("username", "password", "hostname", 2);
+                
+                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2030-01-01T00:00:00Z"});
                 traditionalClient.Refresh();
                 httpTest.ShouldHaveCalled($"https://hostname/api/v2/refreshToken")
                     .WithVerb(HttpMethod.Get)
                     .Times(1);
+            }
+        }
+
+        [Test]
+        public void TestRefreshThrowsException()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest.RespondWithJson(new {accessToken = "faketoken", expires = "2030-01-01T00:00:00Z"});
+                var traditionalClient = new TraditionalClient("username", "password", "hostname", 2);
+
+                httpTest.RespondWith("unauthorized", 401);
+                try {
+                    traditionalClient.Refresh();
+                    Assert.Fail();
+                } catch {
+                    httpTest.ShouldHaveCalled($"https://hostname/api/v2/refreshToken")
+                    .WithVerb(HttpMethod.Get)
+                    .Times(1);
+                }
             }
         }
     }
