1bb8be447f241d8db9db236678c9791d06517e31
Mugesh Shanmugavel <mugesh.shanmugavel@jci.com>
2019-10-07 18:14:59 +0530

    Added ReadPropertyMultiple method

--- MetasysServicesCom/LegacyMetasysClient.cs
+++ MetasysServicesCom/LegacyMetasysClient.cs
@@ -58,9 +58,34 @@ namespace JohnsonControls.Metasys.ComServices
             var response = client.ReadProperty(fqr, property);
             reliability = response.Reliability;
             numericValue = response.NumericValue;
-            priority = response.Priority;       
+            priority = response.Priority;
             rawValue = response.NumericValue;
             return response.StringValue;
+        }
+
+        /// <summary>
+        /// Read many attribute values given the Guids of the objects.
+        /// </summary>
+        /// <param name="reference"></param>
+        /// <param name="objectList"></param>
+        /// <param name="propertyList"></param>
+        /// <param name="valueList"></param>
+        public List<string> ReadPropertyMultiple(string reference, string[] objectList, string[] propertyList, ref List<string> valueList)
+        {
+            var guidList = new List<Guid>();
+            foreach (var guid in objectList)
+            {
+                guidList.Add(GetObjectIdentifier(guid));
+            }
+            var response = client.ReadPropertyMultiple(guidList, propertyList);
+            foreach (var value in response)
+            {
+                foreach (var attributeValue in value.Variants)
+                {
+                    valueList.Add(attributeValue.StringValue);
+                }
+            }
+            return valueList;
         }
     }
 }
