ca457bc1a3ba6951a8989e13fc4bcab06dfdf018
Emanuele Buldrini <jbuldre@jci.com>
2019-10-30 11:42:16 +0100

    Updated XML Documentation for COM and aligned parameters names with basic services.

--- MetasysServicesCom/Interfaces/ILegacyMetasysClient.cs
+++ MetasysServicesCom/Interfaces/ILegacyMetasysClient.cs
@@ -1,25 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JohnsonControls.Metasys.ComServices
 {
+    /// <summary>
+    /// A COM HTTP client for consuming the most commonly used endpoints of the Metasys API.
+    /// </summary>
     [ComVisible(true)]
     [Guid("B1AF1A67-42A0-4E4A-8A07-97AA53B42D02")]
     [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
     public interface ILegacyMetasysClient
     {
-        void login(string hostname, string username, string password);
-        IComVariant ReadProperty(string id, string property);
-        object ReadPropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] objectIdList, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] propertyList);
+        /// <summary>
+        /// Attempts to login to the given host.
+        /// </summary>
+        void TryLogin(string hostname, string username, string password, bool refresh = true);
+        /// <summary>
+        /// Read one attribute value given the Guid of the object.
+        /// </summary>
+        IComVariant ReadProperty(string id, string attributeName);
+        /// <summary>
+        /// Read many attribute values given the Guids of the objects.
+        /// </summary>
+        object ReadPropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] attributeNames);
+        /// <summary>
+        /// Write a single attribute given the Guid of the object. 
+        /// </summary>
         void WriteProperty(string id, string attributeName, string newValue, string priority = null);
-        void WritePropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] attributes, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] values, string priority = null);
-        void SendCommand(string id, string command, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] values = null);       
-        object GetNetworkDevices();
+        /// <summary>
+        /// Write to many attribute values given the Guids of the objects.
+        /// </summary>
+        void WritePropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] attributes, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] attributeValues, string priority = null);
+        /// <summary>
+        /// Send a command to an object.
+        /// </summary>
+        void SendCommand(string id, string command, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] string[] values = null);
+        /// <summary>
+        /// Gets all network devices.
+        /// </summary>
+        object GetNetworkDevices(string type=null);
+        /// <summary>
+        /// Gets all child objects given a parent Guid.
+        /// Level indicates how deep to retrieve objects.
+        /// </summary>
         object GetObjects(string id, int levels = 1);
+        /// <summary>
+        /// Given the Item Reference of an object, returns the object identifier.
+        /// </summary>
         string GetObjectIdentifier(string itemReference);
     }
 }
--- MetasysServicesCom/LegacyMetasysClient.cs
+++ MetasysServicesCom/LegacyMetasysClient.cs
@@ -1,30 +1,37 @@
 ﻿using AutoMapper;
 using JohnsonControls.Metasys.BasicServices;
-using Microsoft.TeamFoundation.Common.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JohnsonControls.Metasys.ComServices
 {
+    /// <summary>
+    /// An COM HTTP client for consuming the most commonly used endpoints of the Metasys API.
+    /// </summary>
     [ComVisible(true)]
     [Guid("0261C5FD-2473-4EC1-A78C-31A5C27C8163")]
     [ClassInterface(ClassInterfaceType.None)]
     public class LegacyMetasysClient : ILegacyMetasysClient
     {
+        /// <summary>
+        /// Metasys Basic Services client for underlying API calls
+        /// </summary>
         protected IMetasysClient Client;
+        /// <summary>
+        /// Automapper instance for mapping Basic Services structures to registered COM DTO
+        /// </summary>
         protected IMapper Mapper;
 
         /// <summary>
-        /// Creates a new LegacyClient.
-        /// </summary>
+        /// Creates a new LegacyMetasysClient.
+        /// </summary>       
         public LegacyMetasysClient()
         {
             // Defines a Mapper From Basic Services structure to COM
-            Mapper = new MapperConfiguration(cfg => {
+            Mapper = new MapperConfiguration(cfg =>
+            {
                 cfg.CreateMap<MetasysObject, IComMetasysObject>()
                     // This is needed in order to correctly map to generic object reference to array, in order to correctly map to VBA
                     .ForMember(dest => dest.Children, opt => opt.MapFrom(src => Mapper.Map<IComMetasysObject[]>(src.Children)));
@@ -32,25 +39,35 @@ namespace JohnsonControls.Metasys.ComServices
                 cfg.CreateMap<VariantMultiple, IComVariantMultiple>()
                     // This is needed in order to correctly map to generic object reference to array, in order to correctly map to VBA
                     .ForMember(dest => dest.Variants, opt => opt.MapFrom(src => Mapper.Map<IComVariant[]>(src.Variants)));
-            }).CreateMapper(); 
+            }).CreateMapper();
         }
 
         /// <summary>
         /// Attempts to login to the given host.
         /// </summary>
+        /// <returns>Access Token.</returns>
         /// <param name="hostname"></param>
         /// <param name="username"></param>
         /// <param name="password"></param>
-        public void login(string hostname, string username, string password)
+        /// <param name="refresh">Flag to set automatic access token refreshing to keep session active.</param>
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysTokenException"></exception>
+        public void TryLogin(string hostname, string username, string password, bool refresh = true)
         {
             Client = new MetasysClient(hostname);
-            Client.TryLogin(username, password);
+            Client.TryLogin(username, password,refresh);
         }
 
         /// <summary>
-        /// Returns the object identifier (id) of the specified object.
+        /// Given the Item Reference of an object, returns the object identifier.
         /// </summary>
+        /// <remarks>
+        /// The itemReference will be automatically URL encoded.
+        /// </remarks>
+        /// <returns>A Guid representing the id, or an empty Guid if errors occurred.</returns>
         /// <param name="itemReference"></param>
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysGuidException"></exception>
         public string GetObjectIdentifier(string itemReference)
         {
             Guid? response = Client.GetObjectIdentifier(itemReference);
@@ -58,56 +75,68 @@ namespace JohnsonControls.Metasys.ComServices
         }
 
         /// <summary>
-        /// Read one attribute value given the reference of the object.
+        /// Read one attribute value given the Guid of the object.
         /// </summary>
+        /// <returns>
+        /// Variant if the attribute exists, null if does not exist.
+        /// </returns>
         /// <param name="id"></param>
-        /// <param name="property"></param>       
-        public IComVariant ReadProperty(string id, string property)
+        /// <param name="attributeName"></param>        
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysPropertyException"></exception>
+        public IComVariant ReadProperty(string id, string attributeName)
         {
             // Parse Id and generate GUID
             Guid guid = new Guid(id);
-            var response = Client.ReadProperty(guid, property).Value;
+            var response = Client.ReadProperty(guid, attributeName).Value;
             return Mapper.Map<IComVariant>(response);
         }
 
         /// <summary>
-        /// Read many attribute values given the references of the objects.
+        /// Read many attribute values given the Guids of the objects.
         /// </summary>
-        /// <param name="objectIdList"></param>
-        /// <param name="propertyList"></param>
-        public object ReadPropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] objectIdList, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] propertyList)
+        /// <returns>
+        /// A list of VariantMultiple with all the specified attributes (if existing).        
+        /// </returns>
+        /// <param name="ids"></param>
+        /// <param name="attributeNames"></param>        
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysPropertyException"></exception>
+        public object ReadPropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] attributeNames)
         {
             // Note: MarshalAs decorator is needed for arrays, otherwise will cause a VBA app crash
             // Note: need a generic object as return type in order to map correctly to VBA type array
             var guidList = new List<Guid>();
-            foreach (var id in objectIdList)
+            foreach (var id in ids)
             {
                 guidList.Add(new Guid(id));
             }
-            var response = Client.ReadPropertyMultiple(guidList, propertyList);
+            var response = Client.ReadPropertyMultiple(guidList, attributeNames);
             return Mapper.Map<IComVariantMultiple[]>(response);
         }
 
         /// <summary>
-        /// write attribute values given the references of the objects.
+        /// Write a single attribute given the Guid of the object. 
         /// </summary>
         /// <param name="id"></param>
         /// <param name="attributeName"></param>
         /// <param name="newValue"></param>
-        /// <param name="priority"></param>
-        public void WriteProperty(string id, string attributeName, string newValue, string priority=null)
-        {        
-            Client.WriteProperty(new Guid(id), attributeName, newValue, priority);            
+        /// <param name="priority">Write priority as an enumeration from the writePriorityEnumSet.</param>
+        /// <exception cref="MetasysHttpException"></exception>
+        public void WriteProperty(string id, string attributeName, string newValue, string priority = null)
+        {
+            Client.WriteProperty(new Guid(id), attributeName, newValue, priority);
         }
 
         /// <summary>
-        /// write many attribute values given the references of the objects.
+        /// Write to many attribute values given the Guids of the objects.
         /// </summary>
         /// <param name="ids"></param>
         /// <param name="attributes"></param>
-        /// <param name="values"></param>
-        /// <param name="priority"></param>
-        public void WritePropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] attributes, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] values, string priority = null)
+        /// <param name="attributeValues">The (attribute, value) pairs split in a array.</param>
+        /// <param name="priority">Write priority as an enumeration from the writePriorityEnumSet.</param>
+        /// <exception cref="MetasysHttpException"></exception>
+        public void WritePropertyMultiple([In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] ids, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] attributes, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] attributeValues, string priority = null)
         {
             // Note: MarshalAs decorator is needed when return type is void, otherwise will cause a VBA error on Automation type not supported when passing array
             var guidList = new List<Guid>();
@@ -116,51 +145,59 @@ namespace JohnsonControls.Metasys.ComServices
                 guidList.Add(new Guid(id));
             }
             // Convert positional arrays to Enumerable
-            var valueList = new List<(string,object)>();
-            for (int i=0; i< attributes.Length;i++)
+            var valueList = new List<(string, object)>();
+            for (int i = 0; i < attributes.Length; i++)
             {
-                valueList.Add((attributes[i],values[i]));
+                valueList.Add((attributes[i], attributeValues[i]));
             }
-            Client.WritePropertyMultiple(guidList, valueList, priority);            
+            Client.WritePropertyMultiple(guidList, valueList, priority);
         }
 
-
-       
         /// <summary>
-        /// 
+        /// Send a command to an object.
         /// </summary>
         /// <param name="id"></param>
         /// <param name="command"></param>
         /// <param name="values"></param>
-        public void  SendCommand(string id, string command, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] values=null)
+        /// <exception cref="MetasysHttpException"></exception>
+        public void SendCommand(string id, string command, [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)]string[] values = null)
         {
             // Note: MarshalAs decorator is needed when return type is void, otherwise will cause a VBA error on Automation type not supported when passing array
-            Guid guid = new Guid(id);          
-            Client.SendCommand(guid, command, values?.ToList());           
+            Guid guid = new Guid(id);
+            Client.SendCommand(guid, command, values?.ToList());
         }
 
-
         /// <summary>
-        /// Gets all child objects given a reference.
+        /// Gets all child objects given a parent Guid.
+        /// Level indicates how deep to retrieve objects.
         /// </summary>
+        /// <remarks>
+        /// A level of 1 only retrieves immediate children of the parent object.
+        /// </remarks>
         /// <param name="id"></param>
-        /// <param name="levels"></param>       
+        /// <param name="levels">The depth of the children to retrieve.</param>
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysHttpParsingException"></exception>
+
         public object GetObjects(string id, int levels = 1)
         {
-            Guid guid = new Guid(id); 
-            var res = Client.GetObjects(guid,levels).ToList();  
-            return Mapper.Map<IComMetasysObject[]>(res);     
+            Guid guid = new Guid(id);
+            var res = Client.GetObjects(guid, levels).ToList();
+            return Mapper.Map<IComMetasysObject[]>(res);
         }
 
         /// <summary>
-        /// Gets all device list
-        /// </summary>      
-        public object GetNetworkDevices()
+        /// Gets all network devices.
+        /// </summary>
+        /// <param name="type">Optional type number as a string</param>
+        /// <exception cref="MetasysHttpException"></exception>
+        /// <exception cref="MetasysHttpParsingException"></exception>
+        public object GetNetworkDevices(string type=null)
         {
             // Note: need a generic object as return type in order to map correctly to VBA type array
-            var res = Client.GetNetworkDevices().ToList();
-            return Mapper.Map<IComMetasysObject[]>(res);         
+            var res = Client.GetNetworkDevices(type).ToList();
+            return Mapper.Map<IComMetasysObject[]>(res);
         }
-      
+
     }
 }
