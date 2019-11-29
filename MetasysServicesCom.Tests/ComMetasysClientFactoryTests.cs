83e6bbb78e55750040e32a4327f42bdda646ec13
Mugesh Shanmugavel <mugesh.shanmugavel@jci.com>
2019-11-29 21:23:01 +0530

    Added Unit test and Example App for COM

---
+++ MetasysServicesCom.Tests/ComMetasysClientFactoryTests.cs
@@ -0,0 +1,55 @@
+﻿using JohnsonControls.Metasys.BasicServices;
+using JohnsonControls.Metasys.ComServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MetasysServicesCom.Tests
+{
+    [TestClass]
+    public class ComMetasysClientFactoryTests
+    {
+        private InitializeMethod InitMethod;
+
+        [TestInitialize]
+        public void Init()
+        {
+            InitMethod = new InitializeMethod();
+            InitMethod.MethodInitialize();
+        }
+
+        [TestMethod]
+        [Description("Method for ComMetasysClientFactory")]
+        public void GetLegacyClient_Success()
+        {
+            //Arrange
+            var legacyClientMock = new Mock<ILegacyMetasysClient>();
+            var comMetasysClientFactoryMock = new Mock<IComMetasysClientFactory>();
+            comMetasysClientFactoryMock.Setup(m => m.GetLegacyClient("hostname", false, "V2", null)).Returns(legacyClientMock.Object);
+
+            //Act
+            var response = InitMethod.ComMetasysClientFactory.GetLegacyClient("hostname");
+
+            //Assert
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod]
+        [Description("Method for ComMetasysClientFactory with invalid version")]
+        public void GetLegacyClient_InvalidVersion()
+        {
+            //Arrange
+            var legacyClientMock = new Mock<ILegacyMetasysClient>();
+            var comMetasysClientFactoryMock = new Mock<IComMetasysClientFactory>();
+            comMetasysClientFactoryMock.Setup(m => m.GetLegacyClient("hostname", false, "V3", null)).Throws(new MetasysUnsupportedApiVersion("V3"));
+
+            //Act
+            void Act()
+            {
+                var res = InitMethod.ComMetasysClientFactory.GetLegacyClient("hostname", false, "V3", null);
+            }
+
+            //Assert
+            Assert.ThrowsException<MetasysUnsupportedApiVersion>(Act, "Invalid Version");
+        }
+    }
+}
