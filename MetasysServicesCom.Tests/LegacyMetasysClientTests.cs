15662ff2289ac80190ae799d4013a026332cc7a6
Mugesh Shanmugavel <mugesh.shanmugavel@jci.com>
2019-11-29 12:01:50 +0530

    Added more unit test cases for COM and Example App.

--- MetasysServicesCom.Tests/LegacyMetasysClientTests.cs
+++ MetasysServicesCom.Tests/LegacyMetasysClientTests.cs
@@ -1,4 +1,6 @@
 ﻿using JohnsonControls.Metasys.BasicServices;
+using JohnsonControls.Metasys.ComServices;
+using JohnsonControls.Metasys.ComServices.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
@@ -11,6 +13,13 @@ namespace MetasysServicesCom.Tests
         private const string date = "2030-01-01T00:01:00Z";
         private static readonly DateTime dateTime = DateTime.Parse(date).ToUniversalTime();
         private static readonly string accessToken = "Bearer fakeLoginToken";
+        private static readonly Guid mockid = new Guid("11111111-2222-3333-4444-555555555555");
+        private static readonly Guid mockid2 = new Guid("11111111-2222-3333-4444-555555555556");
+        private const string mockAttributeName = "property";
+        private const string mockAttributeName2 = "property2";
+        private const string mockAttributeName3 = "property3";
+        private const string mockAttributeName4 = "property4";
+        private const string mockAttributeName5 = "property5";
 
         [TestInitialize]
         public void Init()
@@ -18,6 +27,8 @@ namespace MetasysServicesCom.Tests
             InitMethod = new InitializeMethod();
             InitMethod.MethodInitialize();
         }
+
+        #region Login
 
         [TestMethod]
         [Description("Login with correct credentials")]
@@ -48,7 +59,7 @@ namespace MetasysServicesCom.Tests
             }
 
             //Assert
-            Assert.ThrowsException<MetasysHttpException>(Act,"UnAuthorised");
+            Assert.ThrowsException<MetasysHttpException>(Act, "UnAuthorised");
         }
 
         [TestMethod]
@@ -101,5 +112,335 @@ namespace MetasysServicesCom.Tests
             //Assert
             Assert.ThrowsException<MetasysTokenException>(Act, "Expiry date missing");
         }
+
+        #endregion
+
+        #region GetObjectIdentifier
+
+        [TestMethod]
+        public void GetObjectIdentifer_RetursFqr()
+        {
+            //Arrange
+            InitMethod.httpTest.RespondWith($"\"{mockid.ToString()}\"");
+
+            //Act
+            var id = InitMethod.LClient.GetObjectIdentifier("fully:qualified/reference1");
+
+            //Assert
+            Assert.AreEqual(id, mockid.ToString());
+        }
+
+        #endregion
+
+        #region ReadProperty
+
+        [TestMethod]
+        public void ReadProperty()
+        {
+            //Arrange
+            string json = "{\"item\": { \"" + mockAttributeName + "\": 1 }}";
+            InitMethod.httpTest.RespondWith(json);
+
+            //Act
+            dynamic prop = InitMethod.LClient.ReadProperty(mockid.ToString(), mockAttributeName);
+
+            //Assert
+            Assert.AreEqual(prop.Attribute, mockAttributeName);
+            Assert.AreEqual(prop.Id, mockid.ToString());
+        }
+
+        #endregion
+
+        #region ReadPropertyMultiple
+
+        [TestMethod]
+        public void ReadPropertyMultiple()
+        {
+            //Arrange
+            InitMethod.httpTest
+                .RespondWith("{ \"item\": { \"" + mockAttributeName + "\": \"stringvalue\" } }")
+                .RespondWith("{\"item\": { \"" + mockAttributeName2 + "\": 1 }}");
+
+            string[] ids = new string[1];
+            ids[0] = mockid.ToString();
+
+            string[] attributes = new string[2];
+            attributes[0] = mockAttributeName;
+            attributes[1] = mockAttributeName2;
+
+            //Act
+            dynamic properties = InitMethod.LClient.ReadPropertyMultiple(ids, attributes);
+
+            //Assert
+            Assert.IsInstanceOfType(properties, typeof(IComVariantMultiple[]));
+            Assert.IsNotNull(properties);
+            Assert.AreEqual(properties[0].Variants.Length, attributes.Length);
+        }
+
+        #endregion
+
+        #region WriteProperty
+
+        [TestMethod]
+        public void WriteProperty()
+        {
+            //Arrange
+            InitMethod.httpTest.RespondWith("Accepted", 202);
+
+            //Act
+            InitMethod.LClient.WriteProperty(mockid.ToString(), mockAttributeName, "newValue");
+        }
+
+        #endregion
+
+        #region WritePropertyMultiple
+
+        [TestMethod]
+        public void WritePropertyMultiple()
+        {
+            //Arrange
+            InitMethod.httpTest.RespondWith("Accepted", 202);
+
+            string[] ids = new string[2];
+            ids[0] = mockid.ToString();
+            ids[1] = mockid2.ToString();
+
+            string[] attributes = new string[2];
+            attributes[0] = mockAttributeName;
+            attributes[1] = mockAttributeName2;
+
+            string[] attributeValue = new string[2];
+            attributeValue[0] = "New Value1";
+            attributeValue[1] = "New Value2";
+
+            //Act
+            InitMethod.LClient.WritePropertyMultiple(ids, attributes, attributeValue);
+        }
+
+        #endregion
+
+        #region SendCommand
+
+        [TestMethod]
+        public void TestSendCommand()
+        {
+            //Arrange
+            InitMethod.httpTest.RespondWith("OK", 200);
+            string[] values = { "1", "2", "3" };
+
+            //Act
+            InitMethod.LClient.SendCommand(mockid.ToString(), "Adjust", values);
+        }
+
+        [TestMethod]
+        public void TestSendCommandWithNullValues()
+        {
+            //Arrange
+            InitMethod.httpTest.RespondWith("OK", 200);
+
+            //Act
+            InitMethod.LClient.SendCommand(mockid.ToString(), "Adjust", null);
+        }
+
+        #endregion
+
+        #region GetCommands
+
+        [TestMethod]
+        public void TestGetCommands()
+        {
+            //Arrange
+            string command1 = string.Concat("{\"$schema\": \"http://json-schema.org/schema#\",",
+                "\"commandId\": \"Adjust\",",
+                "\"title\": \"Adjust\",",
+                "\"type\": \"array\",",
+                "\"items\": [{",
+                    "\"type\": \"number\",",
+                    "\"title\": \"Value\",",
+                    "\"minimum\": -20.0,",
+                    "\"maximum\": 120.0",
+                    "}],",
+                "\"minItems\": 1,",
+                "\"maxItems\": 1 }");
+            InitMethod.httpTest.RespondWith(string.Concat("[", command1, "]"));
+
+            //Act
+            dynamic command = InitMethod.LClient.GetCommands(mockid.ToString());
+
+            //Assert
+            Assert.IsNotNull(command);
+            Assert.IsInstanceOfType(command, typeof(IComCommand[]));
+        }
+
+        #endregion
+
+        #region GetNetworkDevices
+
+        [TestMethod]
+        public void TestGetNetworkDevices()
+        {
+            //Arrange
+            string device = string.Concat("{",
+                "\"id\": \"", mockid, "\",",
+                "\"itemReference\": \"fully:qualified/reference\",",
+                "\"name\": \"name\",",
+                "\"typeUrl\": \"https://hostname/api/V2/enumSets/508/members/197\",",
+                "\"description\": \"none\",",
+                "\"firmwareVersion\": \"4.0.0.1105\",",
+                "\"ipAddress\": \"\"}");
+
+            InitMethod.httpTest.RespondWith(string.Concat("{",
+                "\"total\": 1,",
+                "\"next\": null,",
+                "\"previous\": null,",
+                "\"items\": [", device, "],",
+                "\"self\": \"https://hostname/api/V2/networkDevices?page=1&pageSize=200&sort=name\"}"));
+
+            //Act
+            dynamic devices = InitMethod.LClient.GetNetworkDevices();
+
+            //Assert
+            Assert.AreEqual(devices[0].ItemReference, "fully:qualified/reference");
+        }
+
+        #endregion
+
+        #region GetNetworkDevicesTypes
+
+        [TestMethod]
+        public void TestGetNetworkDeviceTypes()
+        {
+            //Arrange
+            InitMethod.httpTest
+               .RespondWith(string.Concat("{",
+                   "\"total\": 1,",
+                   "\"items\": [{",
+                       "\"typeUrl\": \"https://hostname/api/V2/enumSets/508/members/185\"",
+                   "}],",
+                   "\"self\": \"https://hostname/api/V2/networkDevices/availableTypes\"}"))
+               .RespondWith(string.Concat("{",
+                   "\"id\": 185,",
+                   "\"description\": \"NAE55-NIE59\",",
+                   "\"self\": \"https://hostname/api/V2/enumSets/508/members/185\",",
+                   "\"setUrl\": \"https://hostname/api/V2/enumSets/508\"}"));
+
+            //Act
+            dynamic types = InitMethod.LClient.GetNetworkDeviceTypes();
+
+            //Assert
+            Assert.AreEqual(types[0].Description, "NAE55-NIE59");
+        }
+
+        #endregion
+
+        #region GetObjects
+
+        [TestMethod]
+        public void TestGetObjects()
+        {
+            //Arrange
+            string obj = string.Concat("{",
+               "\"id\": \"", mockid, "\",",
+               "\"itemReference\": \"fully:qualified/reference\",",
+               "\"name\": \"name\",",
+               "\"description\": \"description\",",
+               "\"typeUrl\": \"https://hostname/api/V2/enumSets/508/members/197\"}");
+            InitMethod.httpTest.RespondWith(string.Concat("{",
+                "\"total\": 1,",
+                "\"next\": null,",
+                "\"previous\": null,",
+                "\"items\": [", obj, "],",
+                $"\"self\": \"https://hostname/api/V2/objects/{mockid}/objects?page=1&pageSize=200&sort=name\"}}"));
+
+            //Act
+            dynamic objects = InitMethod.LClient.GetObjects(mockid.ToString());
+
+            //Assert
+            Assert.AreEqual(objects[0].ItemReference, "fully:qualified/reference");
+        }
+
+        #endregion
+
+        #region GetSpaces
+
+        [TestMethod]
+        public void TestGetSpaces()
+        {
+            //Arrange
+            string space = string.Concat("{",
+                "\"id\": \"", mockid, "\",",
+                "\"itemReference\": \"fully:qualified/reference\",",
+                "\"name\": \"name\",",
+                "\"typeUrl\": \"https://hostname/api/V2/enumSets/1766/members/3\",",
+                $"\"self\": \"https://hostname/api/V2/spaces/{mockid}\"}}");
+            InitMethod.httpTest.RespondWith(string.Concat("{",
+                "\"total\": 1,",
+                "\"next\": null,",
+                "\"previous\": null,",
+                "\"items\": [", space, "],",
+                "\"self\": \"https://hostname/api/V2/spaces?page=1&pageSize=200&sort=name\"}"));
+
+            //Act
+            dynamic spaces = InitMethod.LClient.GetSpaces();
+
+            //Assert
+            Assert.AreEqual(spaces[0].ItemReference, "fully:qualified/reference");
+        }
+
+        #endregion
+
+        #region GetSpacesTypes
+
+        [TestMethod]
+        public void TestGetSpacesTypes()
+        {
+            //Arrange
+            var item = string.Concat("{",
+                    "\"id\": 3,",
+                    "\"description\": \"Room\",",
+                    "\"self\": \"https://hostname/api/V2/enumSets/1766/members/3\",",
+                    "\"setUrl\": \"https://hostname/api/V2/enumSets/1766\"}");
+            InitMethod.httpTest
+                .RespondWith(string.Concat("{",
+                    "\"total\": 1,",
+                    $"\"items\": [{item}],",
+                    "\"self\": \"https://hostname/api/V2/enumSet/1766\"}"));
+
+            //Act
+            dynamic types = InitMethod.LClient.GetSpacesTypes();
+
+            //Assert
+            Assert.AreEqual(types[0].Description, "Room");
+        }
+
+        #endregion
+
+        #region GetEquipments
+
+        [TestMethod]
+        public void TestGetEquipments()
+        {
+            //Arrange
+            string space = string.Concat("{",
+                "\"id\": \"", mockid, "\",",
+                "\"itemReference\": \"fully:qualified/reference\",",
+                "\"name\": \"name\",",
+                "\"typeUrl\": \"https://hostname/api/V2/enumSets/1766/members/3\",",
+                $"\"self\": \"https://hostname/api/V2/equipment/{mockid}\"}}");
+            InitMethod.httpTest.RespondWith(string.Concat("{",
+                "\"total\": 1,",
+                "\"next\": null,",
+                "\"previous\": null,",
+                "\"items\": [", space, "],",
+                "\"self\": \"https://hostname/api/V2/equipment?page=1&pageSize=200&sort=name\"}"));
+
+            //Act
+            dynamic equipments = InitMethod.LClient.GetEquipment();
+
+            //Assert
+            Assert.AreEqual(equipments[0].ItemReference, "fully:qualified/reference");
+        }
+
+        #endregion
     }
 }
---
+++ MetasysServicesComExampleApp/Program.cs
@@ -0,0 +1,219 @@
+﻿using JohnsonControls.Metasys.ComServices;
+using System;
+using System.Collections.Generic;
+
+namespace MetasysServicesComExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comMetasysClientFactory = new ComMetasysClientFactory();
+            Console.WriteLine("Please enter in your credentials." +
+                   "\nRefer to the metasys-server/basic-services-dotnet README if you need help getting started.");
+            Console.Write("Enter the Hostname:");
+            var hostName = Console.ReadLine();
+            Console.Write("Enter the Username:");
+            var userName = Console.ReadLine();
+            Console.Write("Enter the Password:");
+            var password = Console.ReadLine();
+
+            var legacyClient = comMetasysClientFactory.GetLegacyClient(hostName);
+
+            #region Login
+
+            legacyClient.TryLogin(userName, password);
+            Console.WriteLine("Logging In.....");
+            Console.WriteLine("Login Successfull...");
+
+            #endregion
+
+            #region GetObjectIdentifier
+
+            Console.WriteLine("\n\nGetObjectIdentifier...");
+
+            Console.WriteLine("\nIndicate the object you want to run this example code on.");
+            Console.Write("Enter the fully qualified reference of the object (Example: \"site:device/itemReference\"): ");
+            string object1 = Console.ReadLine();
+
+            Console.WriteLine("\nIndicate the second object you want to run this example code on.");
+            Console.Write("Enter the fully qualified reference of the object: ");
+            string object2 = Console.ReadLine();
+
+            string object1Id = legacyClient.GetObjectIdentifier(object1);
+            string object2Id = legacyClient.GetObjectIdentifier(object2);
+
+            Console.WriteLine($"{object1} id: {object1Id}");
+            Console.WriteLine($"{object2} id: {object2Id}");
+
+            string[] ids = new string[2];
+            ids[0] = object1Id;
+            ids[1] = object2Id;
+
+            #endregion
+
+            #region Read Property
+
+            Console.Write("\n!!!Please note ReadProperty will return null if the attribute does not exist, and will cause an exception in this example!!!");
+            Console.Write("\nEnter an attribute of the objects (Examples: name, description, presentValue): ");
+            string attribute1 = Console.ReadLine();
+
+            Console.Write("Enter a second attribute of the objects: ");
+            string attribute2 = Console.ReadLine();
+
+            List<string> attributes = new List<string>() { attribute1, attribute2 };
+
+            Console.WriteLine($"Get {attribute1} property...");
+
+            var result1Attr1 = legacyClient.ReadProperty(object1Id, attribute1);
+            var result2Attr1 = legacyClient.ReadProperty(object2Id, attribute1);
+
+            Console.WriteLine($"{result1Attr1.Id} {result1Attr1.Attribute} values (string, int, bool, reliability): " +
+                $"\n{result1Attr1.StringValue}, {result1Attr1.NumericValue}, {result1Attr1.BooleanValue}, {result1Attr1.Reliability}");
+            Console.WriteLine($"{result2Attr1.Id} {result2Attr1.Attribute} values (string, int, bool, reliability): " +
+                $"\n{result2Attr1.StringValue}, {result2Attr1.NumericValue}, {result2Attr1.BooleanValue}, {result2Attr1.Reliability}");
+
+            #endregion
+
+            #region ReadPropertyMultiple
+
+            Console.WriteLine("\n\nReadPropertyMultiple...");
+
+            Console.WriteLine($"\nGet {attribute1}, {attribute2} properties for each object...");
+
+            var results = legacyClient.ReadPropertyMultiple(ids, attributes.ToArray());
+
+
+            foreach (var varMultiple in (dynamic)(results))
+            {
+                // Grab the list of Variants for each id
+                var variants = varMultiple.Variants;
+                foreach (var result in variants)
+                {
+                    Console.WriteLine($"{result.Id} {result.Attribute} values (string, int, bool, reliability): " +
+                        $"\n{result.StringValue}, {result.NumericValue},  {result.BooleanValue}, {result.Reliability}");
+                }
+            }
+
+            #endregion
+
+            #region WriteProperty
+
+            Console.Write("\nEnter an attribute of the objects to change (don't worry, this will be reset to it's original value): ");
+            string attribute3 = Console.ReadLine();
+
+            Console.Write("\nEnter one new value for this attribute (this will be applied to both objects): ");
+            string change = Console.ReadLine();
+
+            // Get original values, this code will throw an exception if the 
+            var result1Attr3 = legacyClient.ReadProperty(object1Id, attribute3);
+            var result2Attr3 = legacyClient.ReadProperty(object2Id, attribute3);
+            Console.WriteLine($"{result1Attr3.Id} {result1Attr3.Attribute} original value: {result1Attr3.StringValue}");
+            Console.WriteLine($"{result2Attr3.Id} {result2Attr3.Attribute} original value: {result2Attr3.StringValue}");
+
+            Console.WriteLine("\nWriteProperty...");
+
+            // Change value
+            legacyClient.WriteProperty(object1Id, attribute3, change);
+            System.Threading.Thread.Sleep(1000);
+
+            // View changes
+            var result1Attr3Updated = legacyClient.ReadProperty(object1Id, attribute3);
+            Console.WriteLine($"{result1Attr3Updated.Id} {result1Attr3Updated.Attribute} updated value: {change}");
+
+            // Reset value
+            legacyClient.WriteProperty(object1Id, attribute3, result1Attr3.StringValue);
+            System.Threading.Thread.Sleep(1000);
+
+            #endregion
+
+            #region Write Property Multiple
+
+            Console.WriteLine("\nWrite Property Multiple...");
+
+            // Change value
+            string[] attributes2 = new string[1];
+            attributes2[0] = attribute3;
+            Console.Write("\nEnter one new value for this attribute (this will be applied to both objects): ");
+            string[] changes = new string[2];
+            changes[0] = Console.ReadLine();
+            changes[1] = changes[0];
+            legacyClient.WritePropertyMultiple(ids, attributes2, changes);
+            System.Threading.Thread.Sleep(1000);
+
+            // View changes
+            var result1Attr3UpdatedM = legacyClient.ReadProperty(object1Id, attribute3);
+            var result2Attr3UpdatedM = legacyClient.ReadProperty(object2Id, attribute3);
+            Console.WriteLine($"{result1Attr3UpdatedM.Id} {result1Attr3UpdatedM.Attribute} updated value: {changes[0]}");
+            Console.WriteLine($"{result2Attr3UpdatedM.Id} {result2Attr3UpdatedM.Attribute} updated value: {changes[1]}");
+            System.Threading.Thread.Sleep(1000);
+
+            // Reset Values
+            legacyClient.WriteProperty(object1Id, attribute3, result1Attr3.StringValue);
+            legacyClient.WriteProperty(object2Id, attribute3, result2Attr3.StringValue);
+
+            #endregion
+
+            #region Commands
+
+            Console.WriteLine("\n\nGetCommands...");
+
+            Console.WriteLine($"{object1Id} Commands:");
+
+            dynamic commands = legacyClient.GetCommands(object1Id);
+
+            if (commands.Length != null )
+            {
+               // Console.WriteLine($"{commands.Count()}\n");
+                foreach (dynamic command in (dynamic)commands)
+                {
+                    Console.WriteLine($"{command.Title}\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No commands found.");
+            }
+
+            #endregion
+
+            #region GetNetworkDevices
+
+            Console.WriteLine("\n\nGetNetworkDevices...");
+
+            var types = legacyClient.GetNetworkDeviceTypes();
+            foreach (var type in (dynamic)types)
+            {
+                Console.WriteLine($"\nAvailable Type {type.Id}: {type.Description}, {type.DescriptionEnumerationKey}");
+                string typeId = type.Id.ToString();
+                dynamic devices = legacyClient.GetNetworkDevices(typeId);
+                Console.WriteLine($"Devices found: {devices.Length}");
+                Console.WriteLine($"First Device: {devices[0].Name}");
+            }
+
+            #endregion
+
+            #region GetSpaces
+
+            Console.WriteLine("\n\nGetSpaces...");
+            dynamic spaceTypes = legacyClient.GetSpacesTypes();
+            foreach (var type in (dynamic)spaceTypes)
+            {
+                Console.WriteLine($"\nType {type.Id}: {type.Description}, ");
+            }
+            // Select a space type
+            Console.WriteLine("\nPlease enter the Space Type ID to retrieve all related spaces:");
+            string spaceType = Console.ReadLine();
+            dynamic spaces = legacyClient.GetSpaces(spaceType);
+            Console.WriteLine($"Spaces found: {spaces.Length}");
+            foreach (var space in (dynamic)spaces)
+            {
+                Console.WriteLine($"\n{space.Id}: {space.Name}, {space.ItemReference}");
+            }
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
