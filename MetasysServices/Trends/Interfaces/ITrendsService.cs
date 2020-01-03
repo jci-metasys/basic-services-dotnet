ca536ed23cc9cc567b73d53b35c8647efea3302e
Emanuele Buldrini <jbuldre@jci.com>
2020-01-03 14:49:56 +0100

    Added generic method to read a tree object using a parent/child navigation.

---
+++ MetasysServices/Trends/Interfaces/ITrendsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    public interface ITrendsService
+    {
+        List<Guid>GetTrendedAttributes(Guid id);
+        List<Sample> GetSamples(Guid objectId, Guid attributeId, DateTime startTime, DateTime endTime);
+    }
+}
---
+++ MetasysServices/Trends/Models/Sample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// DTO for Sample
+    /// </summary>
+    public class Sample
+    {
+        public double Value { get; set; }
+        public string UnitsUrl { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsReliable { get; set; }
+    }
+}
