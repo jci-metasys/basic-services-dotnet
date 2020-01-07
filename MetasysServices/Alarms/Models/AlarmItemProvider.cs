c3b3f5bc927024ab0abbbbe78eadb45574ece7e6
gunjan4kumar <gunjan4kumar@gmail.com>
2020-01-07 03:56:00 -0600

    Implement changes to get alarms

---
+++ MetasysServices/Alarms/Models/AlarmItemProvider.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// Provides alarm Item
+    /// </summary>
+    public class AlarmItemProvider : IProvideAlarmItem
+    {
+        /// <summary>
+        /// URI that points back to this resource
+        /// </summary>
+        public string Self { get; set; }
+
+        /// <summary>
+        /// Alarm Unique Identifier (GUID)
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Item fully qualified reference
+        /// </summary>
+        public string ItemReference { get; set; }
+
+        /// <summary>
+        /// Item name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Alarm message
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Is acknowledge required for alarm
+        /// </summary>
+        public bool IsAckRequired { get; set; }
+
+        /// <summary>
+        /// Alarm type route
+        /// </summary>
+        public string TypeUrl { get; set; }
+
+        /// <summary>
+        /// Alarm priority
+        /// </summary>
+        public int Priority { get; set; }
+
+        /// <summary>
+        /// Alarm trigger value details
+        /// </summary>
+        public dynamic TriggerValue { get; set; }
+
+        /// <summary>
+        /// Alarm creation time
+        /// </summary>
+        public string CreationTime { get; set; }
+
+        /// <summary>
+        /// Is alarm already acknowledged
+        /// </summary>
+        public bool IsAcknowledged { get; set; }
+
+        /// <summary>
+        /// Is alarm discarded
+        /// </summary>
+        public bool IsDiscarded { get; set; }
+
+        /// <summary>
+        /// Alarm Category route
+        /// </summary>
+        public string CategoryUrl { get; set; }
+
+        /// <summary>
+        /// Link to Point
+        /// </summary>
+        public string ObjectUrl { get; set; }
+
+        /// <summary>
+        /// Link to annotations
+        /// </summary>
+        public string AnnotationsUrl { get; set; }
+    }
+}
---
+++ MetasysServices/Alarms/Models/PagedResult.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// Paged response of Type T
+    /// </summary>
+    public class PagedResult<T>
+    {
+        /// <summary>
+        /// Total alarms count
+        /// </summary>
+        [JsonProperty(Order = 1)]
+        public int Total { get; set; }
+
+        /// <summary>
+        /// Next page link
+        /// </summary>
+        [JsonProperty(Order = 2)]
+        public string Next { get; set; }
+
+        /// <summary>
+        /// previous page link
+        /// </summary>
+        [JsonProperty(Order = 3)]
+        public string Previous { get; set; }
+
+        /// <summary>
+        /// Pages result of alarm items
+        /// </summary>
+        [JsonProperty(Order = 4)]
+        public T Items { get; set; }
+
+        /// <summary>
+        /// Route information for self
+        /// </summary>
+        [JsonProperty(Order = 5)]
+        public string Self { get; set; }
+
+
+        /// <summary>
+        /// Constructor - Initialize default values
+        /// </summary>
+        public PagedResult()
+        {
+            Next = null;
+            Previous = null;
+            Self = null;
+        }
+
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="pagedResult"></param>
+        public PagedResult(PagedResult<T> pagedResult)
+        {
+            Total = pagedResult.Total;
+            Next = pagedResult.Next;
+            Previous = pagedResult.Previous;
+            Items = pagedResult.Items;
+        }
+    }
+}
