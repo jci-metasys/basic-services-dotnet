c12f05dcb259486330f76139e8b343d275232253
Emanuele Buldrini <jbuldre@jci.com>
2020-01-14 12:30:16 +0100

    Added tests for Alarms Object and Network Devices.

--- MetasysServices.Tests/MetasysClientTestsBase.cs
+++ MetasysServices.Tests/MetasysClientTestsBase.cs
@@ -20,6 +20,38 @@ namespace MetasysServices.Tests
         protected MetasysClient client;
         protected HttpTest httpTest;
 
+        /// <summary>
+        /// Default alarm filter by one month.
+        /// </summary>
+        protected AlarmFilter AlarmFilter = new AlarmFilter
+        {
+            StartTime = new DateTime(2019, 12, 12).ToString(),
+            EndTime = new DateTime(2020, 1, 12).ToString()
+        };
+
+        // Sample alarm response pasted from Postman response
+        protected string Alarm = @"{
+                ""self"": ""https://win2016-vm2/api/v2/alarms/ddbd866f-687f-41ac-b484-aa52669e7381"",
+                ""id"": ""ddbd866f-687f-41ac-b484-aa52669e7381"",
+                ""itemReference"": ""Win2016-VM2:vNAE2343947"",
+                ""name"": ""vNAE2343947"",
+                ""message"": ""Win2016-VM2:vNAE2343947 is offline"",
+                ""isAckRequired"": true,
+                ""typeUrl"": ""https://win2016-vm2/api/v2/enumSets/108/members/71"",
+                ""priority"": 106,
+                ""triggerValue"": {
+                                ""value"": """",
+                    ""unitsUrl"": ""https://win2016-vm2/api/v2/enumSets/507/members/95""
+                },
+                ""creationTime"": ""2019-08-07T17:47:38Z"",
+                ""isAcknowledged"": false,
+                ""isDiscarded"": false,
+                ""categoryUrl"": ""https://win2016-vm2/api/v2/enumSets/33/members/5"",
+                ""objectUrl"": ""https://win2016-vm2/api/v2/objects/4e24edf1-3503-5784-99bc-6bb4707d587d"",
+                ""annotationsUrl"": ""https://win2016-vm2/api/v2/alarms/ddbd866f-687f-41ac-b484-aa52669e7381/annotations""
+            }";
+
+
         [OneTimeSetUp]
         public void Init()
         {
