d959cc7b47546ffe6d383c0d899efc96a1fded89
Gunjan Kumar <gunjan.kumar@jci.com>
2020-02-06 14:10:19 +0530

    Implement Audit feature for SDK

---
+++ MetasysServices/Audits/Models/AuditItemProvider.cs
@@ -0,0 +1,140 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// Provides audit Item
+    /// </summary>
+    public class AuditItemProvider : IProvideAuditItem
+    {
+
+        /// <summary>
+        /// The identifier of the audit(GUID).
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The dateTime representing the creation time when this audit message was created.
+        /// </summary>
+        public string CreationTime { get; set; }
+
+        /// <summary>
+        /// The action performed that initiated the audit.
+        /// https://{hostname}/api/v2/enumSets/577/members for possible values
+        /// </summary>
+        public string ActionTypeUrl { get; set; }
+
+        /// <summary>
+        /// Indicates if the audit has been discarded.
+        /// </summary>
+        public bool Discarded { get; set; }
+
+        /// <summary>
+        /// Enumeration representing status.
+        /// https://{hostname}/api/v2/enumSets/516/members for possible values
+        /// </summary>
+        public string StatusUrl { get; set; }
+
+        /// <summary>
+        /// Data value prior to the Audit.
+        /// </summary>
+        public dynamic PreData { get; set; }
+
+        /// <summary>
+        /// Data value after the Audit.
+        /// </summary>
+        public dynamic PostData { get; set; }
+
+        /// <summary>
+        /// Parameters for the Audit.
+        /// </summary>
+        public dynamic Parameters { get; set; }
+
+        /// <summary>
+        /// The error that may have occurred during an audit.
+        /// </summary>
+        public string ErrorString { get; set; }
+
+        /// <summary>
+        /// The userName of the user that initiated the audit.
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// The user who created this audit
+        /// </summary>
+        public dynamic Signature { get; set; }
+
+        /// <summary>
+        /// A link to the object on which the activity was generated.
+        /// </summary>
+        public string ObjectUrl { get; set; }
+
+        /// <summary>
+        /// Link to annotations.
+        /// </summary>
+        public string AnnotationsUrl { get; set; }
+
+        /// <summary>
+        /// Metasys specific data.
+        /// </summary>
+        public dynamic Legacy { get; set; }
+
+        /// <summary>
+        /// URI that points back to this resource
+        /// </summary>
+        public string Self { get; set; }
+
+        /// <summary>
+        /// Returns a value indicating whether this instance has values equal to a specified object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is AuditItemProvider)
+            {
+                var o = (AuditItemProvider)obj;
+                // Compare each properties one by one for better performance
+                return this.Id == o.Id && this.CreationTime == o.CreationTime && this.ActionTypeUrl == o.ActionTypeUrl
+                                  && this.Discarded == o.Discarded && this.StatusUrl == o.StatusUrl
+                                  && this.ErrorString == o.ErrorString && this.User == o.User
+                                  && this.ObjectUrl == o.ObjectUrl && this.AnnotationsUrl == o.AnnotationsUrl
+                                  && this.Self == o.Self;
+            }
+            return false;
+        }
+
+        /// <summary></summary>
+        public override int GetHashCode()
+        {
+            var code = 13;
+            // Calculate hash on each properties one by one
+            code = (code * 7) + Id.GetHashCode();
+            if (CreationTime != null)
+                code = (code * 7) + CreationTime.GetHashCode();
+            if (this.ActionTypeUrl != null)
+                code = (code * 7) + ActionTypeUrl.GetHashCode();
+            if (this.Discarded != null)
+                code = (code * 7) + Discarded.GetHashCode();
+            if (this.StatusUrl != null)
+                code = (code * 7) + StatusUrl.GetHashCode();
+            if (this.ErrorString != null)
+                code = (code * 7) + ErrorString.GetHashCode();
+            if (this.User != null)
+                code = (code * 7) + User.GetHashCode();
+            if (this.ObjectUrl != null)
+                code = (code * 7) + ObjectUrl.GetHashCode();
+            if (this.AnnotationsUrl != null)
+                code = (code * 7) + AnnotationsUrl.GetHashCode();
+            if (this.Self != null)
+                code = (code * 7) + Self.GetHashCode();
+            return code;
+        }
+    }
+}
---
+++ MetasysServices/Logging/Log4Net.cs
@@ -0,0 +1,190 @@
+﻿
+using System;
+using log4net;
+using log4net.Config;
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Scope = "member", Target = "JohnsonControls.Metasys.BasicServices.Log4NetLog..cctor()")]
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// The Logger class is used by applications to log messages to a common location
+    /// </summary>
+    public sealed class Log4NetLog : ILog
+    {
+        private readonly log4net.ILog log4Net;
+        private const string ConfigFileName = "MetasysService.Logging.config";
+
+        /// <summary>
+        /// Constructs a logger for the specified source.
+        /// </summary>
+        /// <param name="source">
+        /// The source of the log message. Typically the fully-qualified name of the class
+        /// the log messages.
+        /// </param>
+        public Log4NetLog(String source)
+        {
+            log4Net = LogManager.GetLogger(source);
+        }
+
+        #region ILog Members
+
+        /// <summary>
+        /// Create a message in the log
+        /// </summary>
+        /// <param name="level">The level of detail associated with this message</param>
+        /// <param name="message">Text to be logged</param>
+        public void Write(Level level, String message)
+        {
+            Write(level, message, null);
+        }
+
+        /// <summary>
+        /// Create a message in the log
+        /// </summary>
+        /// <param name="level">The level of detail associated with this message</param>
+        /// <param name="message">Text to be logged</param>
+        /// <param name="exception">The exception that caused us to create this message</param>
+        public void Write(Level level, String message, Exception exception)
+        {
+            switch (level)
+            {
+                case Level.Error:
+                    log4Net.Error(message, exception);
+                    break;
+                case Level.Warning:
+                    log4Net.Warn(message, exception);
+                    break;
+                case Level.Info:
+                    log4Net.Info(message, exception);
+                    break;
+                case Level.Verbose:
+                    log4Net.Debug(message, exception);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether writing at the specified level would result in an 
+        /// actual log message being generated.
+        /// </summary>
+        /// <param name="level">The level to check.</param>
+        /// <returns><b>True</b> if a log message would be generated.</returns>
+        public bool IsEnabled(Level level)
+        {
+            switch (level)
+            {
+                case Level.Error:
+                    return log4Net.IsErrorEnabled;
+                case Level.Warning:
+                    return log4Net.IsWarnEnabled;
+                case Level.Info:
+                    return log4Net.IsInfoEnabled;
+                case Level.Verbose:
+                    return log4Net.IsDebugEnabled;
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns <b>True</b> if a error messages would actually be generated.
+        /// </summary>
+        public bool IsError { get { return IsEnabled(Level.Error); } }
+
+        /// <summary>
+        /// Returns <b>True</b> if a warning messages would actually be generated.
+        /// </summary>
+        public bool IsWarning { get { return IsEnabled(Level.Warning); } }
+
+        /// <summary>
+        /// Returns <b>True</b> if a info messages would actually be generated.
+        /// </summary>
+        public bool IsInfo { get { return IsEnabled(Level.Info); } }
+
+        /// <summary>
+        /// Returns <b>True</b> if a verbose messages would actually be generated.
+        /// </summary>
+        public bool IsVerbose { get { return IsEnabled(Level.Verbose); } }
+
+        /// <summary>
+        /// Write a <see cref="Level.Error" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        public void Error(String message)
+        {
+            Write(Level.Error, message, null);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Error" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        /// <param name="exception">Exception to be logged.</param>
+        public void Error(String message, Exception exception)
+        {
+            Write(Level.Error, message, exception);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Warning" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        public void Warning(String message)
+        {
+            Write(Level.Warning, message, null);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Warning" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        /// <param name="exception">Exception to be logged.</param>
+        public void Warning(String message, Exception exception)
+        {
+            Write(Level.Warning, message, exception);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Info" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        public void Info(String message)
+        {
+            Write(Level.Info, message, null);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Info" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        /// <param name="exception">Exception to be logged.</param>
+        public void Info(String message, Exception exception)
+        {
+            Write(Level.Info, message, exception);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Verbose" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        public void Verbose(String message)
+        {
+            Write(Level.Verbose, message, null);
+        }
+
+        /// <summary>
+        /// Write a <see cref="Level.Verbose" /> message in the log.
+        /// </summary>
+        /// <param name="message">Message to be logged.</param>
+        /// <param name="exception">Exception to be logged.</param>
+        public void Verbose(String message, Exception exception)
+        {
+            Write(Level.Verbose, message, exception);
+        }
+
+        #endregion
+    }
+}
