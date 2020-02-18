6fae1aeeb593eda7fc12c00d9dc3ee82df31e8b2
Emanuele Buldrini <jbuldre@jci.com>
2020-02-18 14:32:10 +0100

    Removed Models/Interface structure for microservices.

---
+++ MetasysServices/Alarms/AlarmFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// Filters to get alarms
+    /// </summary>
+    public class AlarmFilter : TimeFilter, IFilterAlarm
+    {       
+        /// <summary>
+        /// The inclusive priority range, from 0 to 255, of the alarm.
+        /// </summary>
+        public string PriorityRange { get; set; }
+
+        /// <summary>
+        /// The type of the requested alarms.
+        /// </summary>
+        public int? Type { get; set; }
+
+        /// <summary>
+        /// The flag to exclude pending alarms Default: false.
+        /// </summary>
+        public bool? ExcludePending { get; set; }
+
+        /// <summary>
+        /// The flag to exclude acknowledged alarms Default: false.
+        /// </summary>
+        public bool? ExcludeAcknowledged { get; set; }
+
+        /// <summary>
+        /// The flag to exclude discarded alarms Default: false.
+        /// </summary>
+        public bool? ExcludeDiscarded { get; set; }
+
+        /// <summary>
+        /// The attribute of the requested alarms.
+        /// </summary>
+        public int? Attribute { get; set; }
+
+        /// <summary>
+        /// The system category of the requested alarms.
+        /// </summary>
+        public int? Category { get; set; }      
+    }
+}
---
+++ MetasysServices/Alarms/AlarmItemProvider.cs
@@ -0,0 +1,144 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace JohnsonControls.Metasys.BasicServices
+{
+    /// <summary>
+    /// Provides alarm Item
+    /// </summary>
+    public class AlarmItemProvider : IProvideAlarmItem
+    {
+        /// <summary>
+        /// URI that points back to this resource
+        /// </summary>
+        public string Self { get; set; }
+
+        /// <summary>
+        /// Alarm Unique Identifier (GUID)
+        /// </summary>
+        /// 
+        [JsonProperty(Required = Required.Always)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Item fully qualified reference
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string ItemReference { get; set; }
+
+        /// <summary>
+        /// Item name
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Alarm message
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Is acknowledge required for alarm
+        /// </summary>
+        public bool IsAckRequired { get; set; }
+
+        /// <summary>
+        /// Alarm type route
+        /// </summary>
+        public string TypeUrl { get; set; }
+
+        /// <summary>
+        /// Alarm priority
+        /// </summary>
+        public int Priority { get; set; }
+
+        /// <summary>
+        /// Alarm trigger value details
+        /// </summary>
+        public dynamic TriggerValue { get; set; }
+
+        /// <summary>
+        /// Alarm creation time
+        /// </summary>
+        public string CreationTime { get; set; }
+
+        /// <summary>
+        /// Is alarm already acknowledged
+        /// </summary>
+        public bool IsAcknowledged { get; set; }
+
+        /// <summary>
+        /// Is alarm discarded
+        /// </summary>
+        public bool IsDiscarded { get; set; }
+
+        /// <summary>
+        /// Alarm Category route
+        /// </summary>
+        public string CategoryUrl { get; set; }
+
+        /// <summary>
+        /// Link to Point
+        /// </summary>
+        public string ObjectUrl { get; set; }
+
+        /// <summary>
+        /// Link to annotations
+        /// </summary>
+        public string AnnotationsUrl { get; set; }
+
+        /// <summary>
+        /// Returns a value indicating whether this instance has values equal to a specified object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is AlarmItemProvider)
+            {
+                var o = (AlarmItemProvider)obj;
+                // Compare each properties one by one for better performance
+                return this.Id == o.Id && this.IsAcknowledged == o.IsAcknowledged && this.IsAckRequired == o.IsAckRequired && this.IsDiscarded == o.IsDiscarded
+                       && this.ItemReference == o.ItemReference && this.Message == o.Message && this.Name == o.Name && this.ObjectUrl == o.ObjectUrl &&
+                       this.Priority == o.Priority && this.Self == o.Self && this.TypeUrl == o.TypeUrl
+                       && this.AnnotationsUrl==o.AnnotationsUrl && this.CategoryUrl==o.CategoryUrl && this.CreationTime==o.CreationTime;
+            }
+            return false;
+        }
+
+        /// <summary></summary>
+        public override int GetHashCode()
+        {
+            var code = 13;
+            // Calculate hash on each properties one by one
+            code = (code * 7) + Id.GetHashCode();
+            if (ItemReference != null)
+                code = (code * 7) + ItemReference.GetHashCode();
+            if (this.AnnotationsUrl != null)
+                code = (code * 7) + AnnotationsUrl.GetHashCode();
+            if (this.CategoryUrl != null)
+                code = (code * 7) + CategoryUrl.GetHashCode();
+            if (this.CreationTime != null)
+                code = (code * 7) + CreationTime.GetHashCode();          
+            code = (code * 7) + IsAckRequired.GetHashCode();          
+            code = (code * 7) + IsAcknowledged.GetHashCode();           
+            code = (code * 7) + IsDiscarded.GetHashCode();
+            if (this.Message != null)
+                code = (code * 7) + Message.GetHashCode();
+            if (this.Name != null)
+                code = (code * 7) + Name.GetHashCode();
+            if (this.ObjectUrl != null)
+                code = (code * 7) + ObjectUrl.GetHashCode();           
+            code = (code * 7) + Priority.GetHashCode();
+            if (this.Self != null)
+                code = (code * 7) + Self.GetHashCode();
+            // to do: add compare on trigger value
+            if (this.TypeUrl != null)
+                code = (code * 7) + TypeUrl.GetHashCode();
+            return code;
+        }
+    }
+}
